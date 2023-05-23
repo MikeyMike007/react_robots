97b603a4a13e44240716b7a7aa8f688f34f6f7d4
Mikael <mikael.hatanpaa@protonmail.com>
2023-05-23 22:43:49 +0200

    Reading robots from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchBox from './searchbox'
 import CardList from './cardlist.jsx';
-import { robots } from './robots.js'
+import Scroll from './scroll.jsx'
 
 
 
@@ -9,24 +9,38 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }))
+  }
+
   onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})
+    this.setState({ searchfield: event.target.value })
   }
 
   render() {
     let filteredRobots = this.state.robots.filter((robot) => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()))
-    return (
-      <div className="tc">
-        <h1>Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-      </div>
-    )
+
+    if (this.state.robots.length === 0)
+      return <h1>Loading</h1>
+    else {
+      return (
+        <div className="tc">
+          <h1>Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      )
+    }
+
   }
 
 }
